dcf4184c7152ba7c5dd7f9ea52307ec6b4b09254
Keanu227 <jkeanu227@gmail.com>
2026-02-02 23:28:17 +0200

    Create page.tsx

---
+++ Rerosperity-NFT-Hub/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh", 
+      background: "linear-gradient(to right, #f9d423, #ff4e50)", 
+      color: "#fff", 
+      fontFamily: "sans-serif" 
+    }}>
+      <h1>✨ Prosperity NFT Hub is Live ✨</h1>
+    </main>
+  )
+}
